6e97c5fc30a333bf9312471ec640821a9947c1cb
Hamza Ramadan <sharinganstrength@hotmail.com>
2022-07-30 20:26:59 +0200

    added google maps api key and map

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,30 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:adzone/theme.dart';
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:adzone/main.dart';
 
 class Map extends StatelessWidget {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-          padding: kDefaultPadding,
-          child: SingleChildScrollView(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                SizedBox(
-                  height: 120,
-                ),
-                Text(
-                  'map',
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.amber,
-                  ),
-                ),
-              ]))),
-    );
+        appBar: AppBar(
+          title: Text("Map"),
+          backgroundColor: Colors.amber[600],
+        ),
+        body: Container(
+          child: GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 11.0,
+              )),
+        ));
   }
 }
